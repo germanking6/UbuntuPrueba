de1b4b1e011b27c9f49c9c837d0d4866e321578c
German <germiss66@gmail.com>
2020-10-29 09:19:56 -0600

    Game semi functional

--- points.py
+++ points.py
@@ -6,8 +6,6 @@ class HomeWork(pg.sprite.Sprite):
         self.points=5
         self.image=pg.image.load("")
         self.pos=self.image.rect.get_rect()
-        self.posAssign()
-    def posAssign(self):
         self.pos.x=random.randrange(900)
         self.pos.y=random.randrange(600)       
 
@@ -16,7 +14,5 @@ class Proyect(pg.sprite.Sprite):
         self.points=15
         self.image=pg.image.load("")
         self.pos=self.image.rect.get_rect()
-        self.posAssign()
-    def posAssign(self):
         self.pos.x=random.randrange(900)
         self.pos.y=random.randrange(600)
