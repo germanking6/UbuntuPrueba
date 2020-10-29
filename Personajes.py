2b5a28abf473d748ce7a8ddc2b79d15ced072427
Joshua Hernandez <joshua.hdz.reza@gmail.com>
2020-10-29 09:00:58 -0700

    create attack fuctions

--- Personajes.py
+++ Personajes.py
@@ -7,7 +7,19 @@ class ken(pg.sprite.Sprite):
         self.image=pg.image.load("ken.png")
         self.pos=self.image.rect.get_rect()
         self.pos.x=random.randrange(900)
-        self.pos.y=random.randrange(600)     
+        self.pos.y=random.randrange(600)
+
+    def attack(self, player_pos_x, player_pos_y):
+        # Find direction vector (dx, dy) between enemy and player.
+        dirvect = pg.math.Vector2(
+            player_pos_x - self.rect.x,
+            player_pos_y - self.rect.y
+        )
+        dirvect.normalize()
+
+        # Move along vector towards the player at current speed.
+        dirvect.scale_to_length(self.speed)
+        self.rect.move_ip(dirvect)
 
 class student(pg.sprite.Sprite):
     def __init__(self):
