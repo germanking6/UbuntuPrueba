6b8b4a8075f9c49c35a7b264ac71e17a5d953e30
Joshua Hernandez <joshua.hdz.reza@gmail.com>
2020-10-28 18:45:47 -0700

    Create tile class it only holds basic values for such obejct

--- src/tile.py
+++ src/tile.py
@@ -1,7 +1,16 @@
 import pygame as pg
+import constant as const
 
 class Tile(pg.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, index):
         super().__init__()
+        self.value = index
+        self.image = pg.image.load(
+            const.IMG_ADDRESS +
+            str(index) +
+            ".png"
+        )
+        self.rect = self.image.get_rect()
 
+        self.image.set_colorkey(const.BLACK)
         
