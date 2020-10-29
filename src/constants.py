499b72c2d3d565659ca947ed629a33e27c0f00fa
Joshua Hernandez <joshua.hdz.reza@gmail.com>
2020-10-28 18:33:46 -0700

    create file for constant values

---
+++ src/constants.py
@@ -0,0 +1,17 @@
+# Colors.
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+# Screen info.
+SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 500
+WIND_SIZE = (SCREEN_HEIGHT, SCREEN_WIDTH)
+PAD_X = 10
+PAD_Y = 10
+
+#Address for img folder
+IMG_ADDRESS = "C:/Users/joshu/OneDrive/Tec/VSC/python/15/icons/"
+
+
+
+NUM_TILES = 16
