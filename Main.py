133f5e72542d1e8a8c226c3a6a3636c02cbd0f72
German <germiss66@gmail.com>
2020-10-29 09:19:56 -0600

    Game semi functional

--- Main.py
+++ Main.py
@@ -1,6 +1,4 @@
-import pygame as pg
 from Juego import Juego
 
-pg.init()
-screen = pg.display.set_mode([900, 600])
-clock = pg.time.Clock()
+game=Juego()
+game.play()
--- Personajes.py
+++ Personajes.py
@@ -6,9 +6,6 @@ class ken(pg.sprite.Sprite):
         super().__init__()
         self.image=pg.image.load("ken.png")
         self.pos=self.image.rect.get_rect()
-        self.posAssign()
-
-    def posAssign(self):
         self.pos.x=random.randrange(900)
         self.pos.y=random.randrange(600)     
 
@@ -17,8 +14,3 @@ class student(pg.sprite.Sprite):
         super().__init__()
         self.image=pg.image.load("")
         self.pos=self.image.rect.get_rect()
-        self.posAssign()
-
-    def posAssign(self):
-        self.pos.x=random.randrange(900)
-        self.pos.y=random.randrange(600)  
